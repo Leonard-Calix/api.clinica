74cd5915f245ffc50f131666bc9394d10d77ea94
Leonard-Calix <bcalixvelasquez@gmail.com>
2022-02-10 18:27:07 -0600

    Modificando puerto

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.set('puerto', process.env.PORT || 4300);
+app.set('port', process.env.PORT || 4300);
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.use(cors())
 
-app.listen(3000, console.log('Server on port 3000'));
+app.listen(app.get('port'), console.log('Server on port ' + app.get('port')));
